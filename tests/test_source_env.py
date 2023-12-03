7af4a6ba5948f6f9b5b098657d597d0167ade952
ds <ds@local>
2023-12-03 13:23:53 +0300

    override pypi priority

--- tests/test_source_env.py
+++ tests/test_source_env.py
@@ -31,3 +31,13 @@ class TestSourceEnv:
         ):
             app_tester.application.reset_poetry()
             assert app_tester.execute("add --dry-run --no-cache aiodns") == 0
+
+    def test_replace_pypi_priority(self, app_tester, config_dir, capsys):
+        with patch.dict(
+            environ,
+            {
+                "POETRY_REPOSITORIES_PYPI_PRIORITY": "secondary",
+            },
+        ):
+            app_tester.application.reset_poetry()
+            assert app_tester.execute("add --dry-run --no-cache aiodns") == 0
