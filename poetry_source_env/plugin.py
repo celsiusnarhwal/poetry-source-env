c6a2dbdb896e30e6c39fe8867c9fdab7d3c8bc94
ds <ds@local>
2023-12-03 13:16:23 +0300

    replace existing repository with provided

--- poetry_source_env/plugin.py
+++ poetry_source_env/plugin.py
@@ -7,7 +7,8 @@ from dict_deep import deep_get
 from poetry.plugins.plugin import Plugin
 from poetry.poetry import Poetry
 from poetry.repositories.legacy_repository import LegacyRepository
-from poetry.repositories.repository_pool import Priority
+from poetry.repositories.repository import Repository
+from poetry.repositories.repository_pool import Priority, RepositoryPool
 from poetry.toml.file import TOMLFile
 from pydantic import BaseSettings, validate_arguments
 from typing_extensions import Self
@@ -29,9 +30,28 @@ class PoetrySourcePlugin(Plugin):
     def activate(self, poetry: Poetry, io: IO = None) -> None:
         config: PSPConfig = PSPConfig.load(poetry.pyproject.file)
 
+        def add_or_replace_repository(
+            pool: RepositoryPool, repo: Repository, priority: Priority
+        ) -> None:
+            # replace DEFAULT priority with PRIMARY
+            if priority is Priority.DEFAULT:
+                for _repo in (
+                    x
+                    for x in pool.all_repositories
+                    if pool.get_priority(x.name) is Priority.DEFAULT
+                ):
+                    pool.remove_repository(_repo.name)
+                    poetry.pool.add_repository(_repo, priority=Priority.PRIMARY)
+
+            # remove repository if already exists
+            if pool.has_repository(repo.name):
+                pool.remove_repository(repo.name)
+            poetry.pool.add_repository(repo, priority=priority)
+
         if config.env:
             repositories = {}
-            pattern = re.compile(rf"{config.prefix}(?P<name>[A-Z_]+)_URL")
+            _prefix = re.escape(config.prefix)
+            pattern = re.compile(rf"{_prefix}(?P<name>[A-Z_]+)_URL")
 
             for env_key in os.environ.keys():
                 match = pattern.match(env_key)
@@ -42,7 +62,10 @@ class PoetrySourcePlugin(Plugin):
                     }
 
             for name, repository in repositories.items():
-                repo = LegacyRepository(name, repository["url"])
+                if name == "pypi":
+                    repo = poetry.pool.repository("PyPI")
+                else:
+                    repo = LegacyRepository(name, repository["url"])
 
                 priority_name = os.getenv(
                     f"{config.prefix}{repository['env_name']}_PRIORITY", "primary"
@@ -57,12 +80,11 @@ class PoetrySourcePlugin(Plugin):
 
                 priority = priorities.get(priority_name.casefold(), Priority.PRIMARY)
 
-                poetry.pool.add_repository(repo, priority=priority)
+                add_or_replace_repository(poetry.pool, repo, priority)
 
         if config.toml:
             for repository in poetry.get_sources():
-                poetry.pool.remove_repository(repository.name)
                 repo = LegacyRepository(
                     expandvars(repository.name), expandvars(repository.url)
                 )
-                poetry.pool.add_repository(repo, priority=repository.priority)
+                add_or_replace_repository(poetry.pool, repo, priority)
